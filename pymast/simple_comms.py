43c726ce6a0cf92a7c78774b7e30736c5db6b9c2
Doug Reichard <doug.reichard@gmail.com>
2023-05-13 23:42:46 -0400

    PyMast new comms

--- pymast/simple_comms.py
+++ pymast/simple_comms.py
@@ -4,7 +4,9 @@ from  sbs_utils.handlerhooks import *
 from sbs_utils.pymast.pymaststory import PyMastStory
 from sbs_utils.pymast.pymasttask import label
 from sbs_utils import query
+from sbs_utils import faces
 from sbs_utils.objects import PlayerShip, Npc
+from sbs_utils.gridobject import GridObject
 
 
 class Story(PyMastStory):
@@ -12,7 +14,8 @@ class Story(PyMastStory):
         super().__init__(*args, **kwargs)
 
         self.start_text = "This is a start project for mast"
-        self.route_comms(self.handle_comms)
+        self.route_comms_select(self.handle_comms)
+        self.route_grid_select(self.damcon_route)
 
     @label()
     def start_server(self):
@@ -25,7 +28,15 @@ class Story(PyMastStory):
         
 
         self.artemis =  query.to_id(PlayerShip().spawn(self.sim, 0,0,0, "Artemis", "tsn", "tsn_battle_cruiser"))
+        faces.set_face(self.artemis, faces.random_terran())
         sbs.assign_client_to_ship(0,self.artemis)
+        
+
+        go1 = GridObject().spawn(self.sim, self.artemis, "fred", "fred", 9,4, 3, "blue", "flint")
+        go2 = GridObject().spawn(self.sim, self.artemis, "barney", "barney", 8,4, 3, "green", "rubble")
+        faces.set_face(go1.id, faces.random_terran())
+        faces.set_face(go2.id, faces.random_terran())
+
 
         print(f"start_server {self.start_text}")
         self.gui_section("area:2,20,80,35;")
@@ -63,32 +74,154 @@ class Story(PyMastStory):
 
     @label()
     def handle_comms(self):
-        def button_hello(story, comms):
-            print("Button")
+        #
+        # This label is called for a player ship (COMMS_ORIGIN_ID)
+        # and the COMMS_SELECTED_ID ship has not been communicated with
+        # this is used to resolve where to START the conversation with the TO ship
+        #
+        # COMMS_SELECTED_ID is the id of the target
+
+        if self.task.COMMS_SELECTED_ID == self.task.COMMS_ORIGIN_ID:
+            # This is the same ship
+            yield self.jump(self.internal_comms)
+        elif query.has_role(self.task.COMMS_SELECTED_ID, 'Station'):
+            yield self.jump(self.comms_station)
+        elif query.has_role(self.task.COMMS_SELECTED_ID, 'raider'):
+            yield self.jump(self.npc_comms)
+
+        # Anything else has no comms buttons
+        # and static as the id
+        #comms_info "static"
+
+    #================ internal_comms ==================
+    @label()
+    def internal_comms(self):
+        #
+        # Setup faces for the departments
+        #
+        self.task.doctor = faces.random_terran()
+        self.task.biologist = faces.random_terran()
+        self.task.counselor = faces.random_terran()
+        self.task.major = faces.random_terran()
+        yield self.jump(self.internal_comms_loop)
+
+    # ================ internal_comms_loop ==================
+    @label()
+    def internal_comms_loop(self):
+        def button_sickbay(story, comms):
+            comms.receive("The crew health is great!", face=story.task.doctor, color="blue", title="sickbay")
+        def button_security(story, comms):
+            comms.receive("All secure", face=story.task.major, color="red", title="security")
+        def button_exobiology(story, comms):
+            comms.receive("Testing running, one moment", face=story.task.biologist, color="green", title="exobiology")
+        def button_counselor(story, comms):
+            comms.receive("Something is disturbing the crew", face=story.task.counselor, color="cyan", title="counselor")
+            yield story.delay(seconds=2, use_sim=True)
+            comms.receive("Things feel like they are getting worse", face=story.task.counselor, color="cyan", title="counselor")
+        
+        yield self.await_comms({
+            "sickbay": button_sickbay,
+            "security": button_security,
+            "exobiology": button_exobiology,
+            "counselor": button_counselor,
+        })
+        # loop
+        yield self.jump(self.internal_comms_loop)
+
+        # #
+        #     + "Exobiology" color "green":
+        #         receive  "Testing running, one moment" title "Exobiology" face biologist color "green"
+        #         # It is best to schedule delayed responses so the comms buttons are not stalled
+        #         schedule test_finished
+        #     + "counselor" color "cyan":
+        #         receive  "Something is disturbing the crew" title "counselor" face counselor color "cyan"
+        #         #
+        #         # but you can delay comms, There will be no buttons during this delay
+        #         #
+        #         delay sim 3s
+        #         receive  "Things feel like they are getting worse" title "counselor" face counselor color "cyan"
+        # end_await
+        # -> internal_comms_loop
+
+    @label()
+    def npc_comms(self):
+
+        def button_hail(story, comms):
+            comms.receive("We will destroy you, disgusting Terran scum!")
+
+        def button_surrender(story, comms):
+            comms.receive("OK we give up")
+
+        yield self.await_comms({
+            "Hail": button_hail,
+            "Surrender": button_surrender
+        })
+        # loop
+        yield self.jump(self.npc_comms)
+
+    @label()
+    def comms_station(self):
+
+        def button_hail(story, comms):
             comms.transmit("Hello")
-            comms.receive("Hello, back")
+            comms.receive("Yo")
 
         yield self.await_comms({
-            "Hello": button_hello
+            "Hail": button_hail
         })
+        # loop
+        yield self.jump(self.comms_station)
+
+    # ================ damcon_route ==================
+    @label()
+    def damcon_route(self):
+        # COMMS_SELECTED_ID is the id of the target
+        if query.has_role(self.task.COMMS_SELECTED_ID, 'flint'):
+            yield self.jump(self.comms_flintstone)
+        elif query.has_role(self.task.COMMS_SELECTED_ID, 'rubble'):
+            yield self.jump(self.comms_rubble)
+
+    # ================ comms_flintstone ==================
+    @label()
+    def comms_flintstone(self):
+        def button_hail(story, comms):
+            comms.broadcast(story.client_id, "Yabba Daba Dooo", "orange")
+            
 
         yield self.await_comms({
-            "Hola": button_hello
+            "Hail": button_hail
         })
+        # -> comms_flintstone
+        yield self.jump(self.comms_flintstone)
+
+    # ================ comms_rubble ==================
+    @label()
+    def comms_rubble(self):
+        def button_hail(story, comms):
+            comms.broadcast(story.client_id, "Hey fred .... how you doin fred?", "brown")
+
+
+        yield self.await_comms({
+            "Hail": button_hail
+        })
+        # -> comms_flintstone
+        yield self.jump(self.comms_rubble)
+
+
 
     @label()
     def start(self):
         # Create the world here
 
         # Create a space station
-        ds1 = Npc().spawn(self.sim, 1000,0,1000, "DS1", "tsn", "starbase_command", "behav_station")
-        ds2 = Npc().spawn(self.sim, 1000,0,-1000, "DS2", "tsn", "starbase_command", "behav_station")
-        query.add_role({ds1.id, ds2.id}, "Station")
+        ds1 = Npc().spawn(self.sim, 1000,0,1000, "DS1", "tsn, Station", "starbase_command", "behav_station")
+        ds2 = Npc().spawn(self.sim, 1000,0,-1000, "DS2", "tsn, Station", "starbase_command", "behav_station")
+        faces.set_face(ds1.id, faces.random_terran())
+        faces.set_face(ds2.id, faces.random_terran())
 
         # Create an enemy
         k001 = Npc().spawn(self.sim, -1000,0,1000, "K001", "raider", "kralien_dreadnaught", "behav_npcship")
-
-        self.schedule_task(self.task_npc_targeting)
+        faces.set_face(k001.id, faces.random_kralien())
 
         sbs.resume_sim()
         yield self.jump(self.end_game)
@@ -134,20 +267,3 @@ class Story(PyMastStory):
         self.gui_console(self.task.console_select)
         self.await_gui()
 
-    @label()    
-    def task_npc_targeting(self):
-        raiders = query.role('raider')
-        if len(raiders)==0:
-            return
-
-        for raider in raiders:
-            the_target = query.closest(raider, query.role("__PLAYER__"), 2000)
-            if the_target is None:
-                the_target = query.closest(raider, query.role("Station"))
-            if the_target is not None:
-                query.target(self.sim, raider, the_target, True)
-
-        yield self.delay(5)
-        yield self.jump(self.task_npc_targeting)
-
-        
