eff81a89d64bde776c09c6c20f0f692640c9f1e5
Doug Reichard <doug.reichard@gmail.com>
2023-09-05 15:45:18 -0400

    skills example

--- script.py
+++ script.py
@@ -2,10 +2,13 @@ import sbslibs
 from  sbs_utils.handlerhooks import *
 from sbs_utils.gui import Gui
 from sbs_utils.pymast.pymaststorypage import PyMastStoryPage
+from sbs_utils.mast.mast import Mast
 from story import Story
 
 class SimpleAiPage(PyMastStoryPage):
     story = Story()
+
+Mast.include_code = True
 
 Gui.server_start_page_class(SimpleAiPage)
 Gui.client_start_page_class(SimpleAiPage)
